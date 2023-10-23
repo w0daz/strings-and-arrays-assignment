f972bef89421f4e43a7b48320ad4079475bea7f8
Gabriel Ogungbade <tamexplora@gmail.com>
2023-10-23 19:10:19 +0300

    completed excercises

--- StringsAndArrays/Program.cs
+++ StringsAndArrays/Program.cs
@@ -60,27 +60,48 @@
             // 1. Basic Array Operations
             // 1.1 initialize array called numbers containing array of int values
             //     Add these values to the array -> 2 3 5 7 11 
+            int[] numbers = { 2, 3, 5, 7, 11 };
 
             // 1.2 Output the numbers array -> hint use Join
             // Expected outcome: 2, 3, 5, 7, 11
+            string numbersString = string.Join(", ", numbers);
+            Console.WriteLine(numbersString);
 
             // 1.3 Output the value of the third (3th) item in the numbers array 
             // Expected outcome: 5
+            Console.WriteLine(numbers[2]);
 
 
             // 2. Array Manipulation
             Console.WriteLine("Please enter five names:");
             // 2.1 Initialize new names string array variable that has room for 5 items
             //     The maximum lenght of the string array should be 5
+            string[] names = new string[5];
 
             // 2.2 Ask the user to input new names 5 times
             //     and assing each name to the names string array in the given order.
             //     The first name should be at index 0
             //     Hint: you might want to use some sort of a loop here
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.Write($"Enter name {i + 1}: ");
+                names[i] = Console.ReadLine();
+            }
 
             // 2.3 Output the array of given names.
+            Console.WriteLine("Names entered:");
+            foreach (string name_ in names)
+            {
+                Console.WriteLine(name_);
+            }
 
             // 2.4 Reverse the order of the items in the names array and outpu the result
+            Array.Reverse(names);
+            Console.WriteLine("Reversed names:");
+            foreach (string name_ in names)
+            {
+                Console.WriteLine(name_);
+            }
 
 
             // 3. Array Searching
@@ -89,21 +110,39 @@
             // 3.1 Try to find out the searchName from the Task 2 names string array
             //     by finding out it's index.
             //     Assign the index value to new position integer variable
+            int position = Array.IndexOf(names, searchName);
 
             // 3.2 Check that the index position was found
             //     Hint: What is the result of IndexOf if nothing is found
             // 3.2.1 If position is found output the searchName and the found position
             // 3.2.2 Else output that the searchName was not found
+            if (position != -1)
+            {
+                Console.WriteLine($"Found {searchName} at position {position}.");
+            }
+       
+            else
+            {
+                Console.WriteLine($"{searchName} was not found.");
+            }
 
             // 4. BONUS
 
             // 1. String to Array
             // 1.1 Create new empty words string array
+            string[] words;
 
             // 1.2 Try to get each word from the helloWorld variable and assing the values to
             //     the created words string array
+            words = helloWorld.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
 
             // 1.3 Output the values of the words array
+            Console.WriteLine("Words in the helloWorld string:");
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
